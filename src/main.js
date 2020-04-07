3325896c82576881332f7b0c7f26eb53983981ff
Daniel Nery <danielnso97@gmail.com>
2020-04-07 03:29:00 -0300

    Add hero art and cards

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Papa from "papaparse";
 import data from "./data/heroes.csv";
 import Tabulator from "tabulator-tables";
+import $ from "jquery";
 
 import "tabulator-tables/dist/css/tabulator.min.css";
 import "./style.scss";
@@ -10,6 +11,7 @@ const parseFile = (rawFile) => {
     Papa.parse(rawFile, {
       download: true,
       header: true,
+      dynamicTyping: true,
       complete: (results) => {
         resolve(results.data);
       },
@@ -17,12 +19,26 @@ const parseFile = (rawFile) => {
   });
 };
 
+let ind = ["c", "r", "e", "l"];
+let elm = ["w", "e", "l", "f", "d"];
+let a = 0;
+let b = 0;
+
+$("#test-btn").click(() => {
+  $(".card-bg").hide();
+  $(".card-elem").hide();
+  $(`#card-bg-${ind[a]}`).show();
+  $(`#card-elem-${elm[b]}`).show();
+  a = (a + 1) % 4;
+  b = (b + 1) % 5;
+});
+
 (async () => {
   let heroes = await parseFile(data);
   console.log(heroes);
 
-  let table = new Tabulator("#table", {
-    data: heroes, //load row data from array
-    autoColumns: true,
-  });
+  //   let table = new Tabulator("#table", {
+  //     data: heroes,
+  //     autoColumns: true,
+  //   });
 })();
