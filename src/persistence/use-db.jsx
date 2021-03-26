cd2b618c20cf66ed9a2631efc3e166c5182a8648
Daniel Nery <danielnso97@gmail.com>
2021-03-26 12:47:27 -0300

    Easter Update 2021 - Add Red

--- src/persistence/use-db.jsx
+++ src/persistence/use-db.jsx
@@ -3,7 +3,7 @@ import LocalStorage from "lowdb/adapters/LocalStorage";
 import { dataToHero } from "../mappers/data-to-hero";
 
 export const db = low(
-  new LocalStorage("cta-grimoire-db-3", {
+  new LocalStorage("cta-grimoire-db-4", {
     deserialize: (data) => {
       let db = JSON.parse(data);
 
